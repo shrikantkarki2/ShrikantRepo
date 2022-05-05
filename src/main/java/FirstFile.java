664d5960b8ccf3dd8b8642e205956ab420737723
Shrikant R Karki <srikantkarki2@gmail.com>
2022-05-05 13:16:58 -0700

    fourth commit

--- src/main/java/FirstFile.java
+++ src/main/java/FirstFile.java
@@ -1,6 +1,6 @@
 public class FirstFile {
     public static void main(String[] args) {
         System.out.println("Hi this is my first git project");
-        System.out.print("The file is now modified");
+        System.out.print("The file is now  newly");
     }
 }
