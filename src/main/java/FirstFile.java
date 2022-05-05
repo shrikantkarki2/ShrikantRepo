b61c7c5a38dcfb4363efdd70b5e9cb1fa7f4fd97
Shrikant R Karki <srikantkarki2@gmail.com>
2022-05-05 13:09:32 -0700

    Second commit

--- src/main/java/FirstFile.java
+++ src/main/java/FirstFile.java
@@ -1,5 +1,6 @@
 public class FirstFile {
     public static void main(String[] args) {
         System.out.println("Hi this is my first git project");
+        System.out.print("The file is now modified");
     }
 }
