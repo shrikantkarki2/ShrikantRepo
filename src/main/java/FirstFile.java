93a81ee0b062aad1668a60c655a2a1141ba21843
Shrikant R Karki <srikantkarki2@gmail.com>
2022-05-06 12:40:55 -0700

    commit to check the push command

--- src/main/java/FirstFile.java
+++ src/main/java/FirstFile.java
@@ -2,5 +2,7 @@ public class FirstFile {
     public static void main(String[] args) {
         System.out.println("Hi this is my first git project");
         System.out.print("The file is now  newly");
+
+        System.out.println("push line");
     }
 }
